b18b7045646fdaa46fafa1e3ce0974febe04b318
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-31 01:05:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,14 +1,24 @@
 # Write your methods here
 
 def loop_message_five_times(array)
-  
+  c = 0
+  while array[c] do
+    puts array[c]
+    c += 1
+  end
 end
 
 def loop_message_n_times(array, n)
-  
+  while array[n] do
+    puts array[c]
+    c += 1
+  end
 end
 
 def output_array(array)
   
 end
 
+def return_string_array(array)
+  
+end
